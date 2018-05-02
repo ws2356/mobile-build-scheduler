902ccc2bbe6e5b3c94b2052fc665f86932fb2528
wansong <betterofsong@gmail.com>
2018-05-02 20:55:45 +0800

    trivial

--- routes/build.js
+++ routes/build.js
@@ -1,35 +1,31 @@
 const express = require('express');
+const redis = require("redis");
 const fs = require('fs');
 const { spawn } = require('child_process');
 
 const { DATA_VOLUME, REDIS_HOST } = config;
 
 const router = express.Router();
+const client = redis.createClient({
+  host: REDIS_HOST,
+});
 
-(() => {
-  var redis = require("redis"),
-  client = redis.createClient({
-    host: REDIS_HOST,
-  });
-
-  // if you'd like to select database 3, instead of 0 (default), call
-  // client.select(3, function() { /* ... */ });
-
-  client.on("error", function (err) {
-    console.log("Error " + err);
-  });
-
-  client.set("string key", "string val", redis.print);
-  client.hset("hash key", "hashtest 1", "some value", redis.print);
-  client.hset(["hash key", "hashtest 2", "some other value"], redis.print);
-  client.hkeys("hash key", function (err, replies) {
-    console.log(replies.length + " replies:");
-    replies.forEach(function (reply, i) {
-      console.log("    " + i + ": " + reply);
-    });
-    client.quit();
-  });
-})();
+// (() => {
+//   client.on("error", function (err) {
+//     console.log("Error " + err);
+//   });
+//
+//   client.set("string key", "string val", redis.print);
+//   client.hset("hash key", "hashtest 1", "some value", redis.print);
+//   client.hset(["hash key", "hashtest 2", "some other value"], redis.print);
+//   client.hkeys("hash key", function (err, replies) {
+//     console.log(replies.length + " replies:");
+//     replies.forEach(function (reply, i) {
+//       console.log("    " + i + ": " + reply);
+//     });
+//     client.quit();
+//   });
+// })();
 
 async function handleTask(req, res, next) {
   const { query, body: { repository: { git_http_url } } } = req;
